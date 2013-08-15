21a3ea795c863646d7dec86a1c08508a66d31302
Shawn Renner <resume@shawnrenner.com>
2013-08-15 14:16:18 -0500

    Start using tests

--- mcptouch.Tests/Controllers/HomeControllerTest.cs
+++ mcptouch.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace mcptouch.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreNotEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
         }
 
         [TestMethod]
---
+++ mcptouch.Tests/Controllers/SecurityCheckTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mcptouch.Helpers;
+
+namespace mcptouch.Tests.Controllers
+{
+    [TestClass]
+    public class SecurityCheckTest
+    {
+        [TestMethod]
+        public void UserOwnsCar()
+        {
+            Assert.IsTrue(SecurityCheck.UserOwnsCar(1, 2));
+            Assert.IsFalse(SecurityCheck.UserOwnsCar(1, 99));
+        }
+    }
+}
